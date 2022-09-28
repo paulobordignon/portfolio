624e66dd287abe1acd1189cc926c2c3c85097f8d
Paulo Bordignon <p.h_@outlook.com>
2022-09-28 01:05:12 -0300

    config(all): added tailwind and structured folders

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <p className="text-3xl">asdasdadsads</p>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
