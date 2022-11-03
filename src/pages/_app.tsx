36ca8a4bfca92014f869fd07e8efe03edd6df90b
Paulo Bordignon <p.h_@outlook.com>
2022-11-03 01:53:55 -0300

    feat(frontend): added session auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,70 @@
 import type { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import "@src/styles/main.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import {
+  GetSiweMessageOptions,
+  RainbowKitSiweNextAuthProvider,
+} from "@rainbow-me/rainbowkit-siwe-next-auth";
+
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Paulo Bordignon's Portfolio",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const getSiweMessageOptions: GetSiweMessageOptions = () => ({
+  statement: "Sign in to the Paulo Bordignon's Portfolio.",
+});
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <SessionProvider refetchInterval={0} session={pageProps.session}>
+        <RainbowKitSiweNextAuthProvider
+          getSiweMessageOptions={getSiweMessageOptions}
+        >
+          <RainbowKitProvider
+            chains={chains}
+            theme={darkTheme({
+              accentColor: "#64ffda",
+              accentColorForeground: "rgba(13, 17, 23, 1)",
+              borderRadius: "medium",
+              fontStack: "system",
+              overlayBlur: "small",
+            })}
+          >
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </RainbowKitSiweNextAuthProvider>
+      </SessionProvider>
+    </WagmiConfig>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,27 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import "@rainbow-me/rainbowkit/styles.css";
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-  darkTheme,
-} from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-
 import { About, Alert, Footer, Header, Hero, Projects } from "@src/components";
 import { AlertProvider } from "@src/providers";
 
-const { chains, provider } = configureChains(
-  [chain.goerli],
-  [publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "Paulo Bordignon's Portfolio",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
 const Home: NextPage = () => {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        chains={chains}
-        theme={darkTheme({
-          accentColor: "#64ffda",
-          accentColorForeground: "rgba(13, 17, 23, 1)",
-          borderRadius: "medium",
-          fontStack: "system",
-          overlayBlur: "small",
-        })}
-      >
-        <Head>
-          <title>Paulo Bordignon</title>
-          <link rel="icon" href="/favicon.png" />
-        </Head>
-        <AlertProvider>
-          <Header />
-          <main className="max-w-7xl mx-auto">
-            <Hero />
-            <About />
-            <Projects />
-          </main>
-          <Footer />
-          <Alert />
-        </AlertProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        <title>Paulo Bordignon</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <AlertProvider>
+        <Header />
+        <main className="max-w-7xl mx-auto">
+          <Hero />
+          <About />
+          <Projects />
+        </main>
+        <Footer />
+        <Alert />
+      </AlertProvider>
+    </>
   );
 };
 
