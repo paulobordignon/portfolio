350de22749bb88bea14aa22cbf52d53793647a09
Paulo Bordignon <p.h_@outlook.com>
2022-12-06 00:24:41 -0300

    fix(admin): error first load

--- src/components/organisms/Admin/Admin.tsx
+++ src/components/organisms/Admin/Admin.tsx
@@ -1,37 +1,28 @@
-import { useEffect, useState } from "react";
+import { createRef, useEffect, useState } from "react";
 import { ethers } from "ethers";
 import abi from "@src/artifacts/contracts/Projects.json";
-/* import { Button, Input } from "@src/components"; */
+import { Button, Input } from "@src/components";
 import { useAlert } from "@src/hooks";
 
 export function Admin() {
   const { addAlert, addVariant } = useAlert();
-  /* const [loading, setLoading] = useState(false); */
+  const [loading, setLoading] = useState(false);
   const [allProjects, setAllProjects] = useState([]);
-  /* const iptImage = createRef<HTMLInputElement>();
+  const iptImage = createRef<HTMLInputElement>();
   const iptTitle = createRef<HTMLInputElement>();
   const iptAbout = createRef<HTMLInputElement>();
   const iptKeywords = createRef<HTMLInputElement>();
   const iptGitHub = createRef<HTMLInputElement>();
-  const iptWebsite = createRef<HTMLInputElement>(); */
+  const iptWebsite = createRef<HTMLInputElement>();
 
   const contractAddress = process.env.NEXT_PUBLIC_CONTRACTADDRESS;
   const contractABI = abi.abi;
   const localProvider = new ethers.providers.Web3Provider(window.ethereum);
-  const alchemyProvider = new ethers.providers.AlchemyProvider(
-    "goerli",
-    process.env.NEXT_PUBLIC_GOERLI_PROVIDER
-  );
-  const listContract = new ethers.Contract(
-    contractAddress,
-    contractABI,
-    alchemyProvider
-  );
-  /* const crudContract = new ethers.Contract(
+  const contract = new ethers.Contract(
     contractAddress,
     contractABI,
     localProvider.getSigner()
-  ); */
+  );
 
   const fields = [
     { label: "Title", placeholder: "Project title" },
@@ -50,34 +41,35 @@ export function Admin() {
     addVariant(title);
   }
 
-  /* const addProject = async () => {
+  const addProject = async () => {
     try {
-      setLoading(true);
+      if (iptTitle.current.value) {
+        setLoading(true);
 
-      const projectTxn = await crudContract.addProject(
-        iptImage.current.value,
-        iptTitle.current.value,
-        iptAbout.current.value,
-        JSON.parse(iptKeywords.current.value),
-        iptGitHub.current.value,
-        iptWebsite.current.value
-      );
+        const projectTxn = await contract.addProject(
+          iptImage.current.value,
+          iptTitle.current.value,
+          iptAbout.current.value,
+          JSON.parse(iptKeywords.current.value),
+          iptGitHub.current.value,
+          iptWebsite.current.value
+        );
 
-      await projectTxn.wait();
+        await projectTxn.wait();
 
-      listAllProjects();
-      showAlert("Success", `Project has been added ${projectTxn.hash}`);
+        listAllProjects();
+        showAlert("Success", `Project has been added ${projectTxn.hash}`);
+      }
     } catch (error) {
       showAlert("Error", error.message);
     } finally {
       setLoading(false);
     }
-  }; */
+  };
 
   const listAllProjects = async () => {
-    console.log("1");
     try {
-      const listProjects = await listContract.getAllProjects();
+      const listProjects = await contract.getAllProjects();
 
       const projectsCleaned = listProjects.map((project) => {
         return {
@@ -96,21 +88,22 @@ export function Admin() {
     }
   };
 
-  /* const removeProject = async (index) => {
+  const removeProject = async (index) => {
     try {
-      const removeProjectTxn = await crudContract.removeProject(index);
+      if (index) {
+        const removeProjectTxn = await contract.removeProject(index);
 
-      await removeProjectTxn.wait();
+        await removeProjectTxn.wait();
 
-      listAllProjects();
-      showAlert("Success", "The project has been removed");
+        listAllProjects();
+        showAlert("Success", "The project has been removed");
+      }
     } catch (error) {
       showAlert("Error", error.message);
     }
-  }; */
+  };
 
   useEffect(() => {
-    console.log("localProvider", localProvider, listContract);
     listAllProjects();
   }, []);
 
@@ -123,19 +116,19 @@ export function Admin() {
               className="basis-full sm:basis-[calc(50%-1.25rem)] w-max"
               key={item.label}
             >
-              {/* <Input
+              <Input
                 label={item.label}
                 placeholder={item.placeholder}
                 ref={eval(`ipt${item.label}`)}
-              /> */}
+              />
             </div>
           );
         })}
-        {/* <Button
+        <Button
           title="Add Project"
           onClick={() => addProject()}
           disabled={loading}
-        /> */}
+        />
       </form>
 
       {allProjects && (
@@ -146,12 +139,12 @@ export function Admin() {
               return (
                 <li key={project.title} className="mt-1">
                   {i} - {project.title}
-                  {/*  <button
+                  <button
                     className="ml-5 border-2 rounded-[10px] p-1 text-sm border-error text-error hover:border-errorHover hover:text-errorHover"
                     onClick={() => removeProject(i)}
                   >
                     Remove
-                  </button> */}
+                  </button>
                 </li>
               );
             })}
