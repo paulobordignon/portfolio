658c53795a33a2bd51ae362104649ef7e4ff745c
Paulo Bordignon <p.h_@outlook.com>
2023-07-20 00:38:00 -0300

    feat(admin): start to use wagmi wallet connection as provider to be possible send transaction

--- src/components/organisms/Admin/Admin.tsx
+++ src/components/organisms/Admin/Admin.tsx
@@ -1,109 +1,114 @@
-import { useEffect, useState, useRef } from "react";
-import { ethers } from "ethers";
-import abi from "@src/artifacts/contracts/Projects.json";
+// @ts-nocheck
+import { useState, useRef, useEffect } from "react";
+import {
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+  useWalletClient,
+} from "wagmi";
+
 import { Button, Input } from "@src/components";
 import { useAlert } from "@src/hooks";
+import abi from "@src/artifacts/contracts/Projects.json";
 
 export function Admin() {
   const { addAlert, addVariant } = useAlert();
-  const [loading, setLoading] = useState(false);
   const [allProjects, setAllProjects] = useState([]);
+  const removeId = useRef<Number>();
+  const createNew = useRef<Boolean>(false);
   const iptTitle = useRef<HTMLInputElement>();
   const iptAbout = useRef<HTMLInputElement>();
   const iptImage = useRef<HTMLInputElement>();
   const iptKeywords = useRef<HTMLInputElement>();
   const iptGitHub = useRef<HTMLInputElement>();
   const iptWebsite = useRef<HTMLInputElement>();
-  const alchemyProvider = useRef<any>();
-  const contract = useRef<any>();
   const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
-  const contractABI = abi.abi;
+
+  const { data: walletClient } = useWalletClient();
+  useContractRead({
+    address: contractAddress,
+    abi: abi.abi,
+    functionName: "getAllProjects",
+    walletClient: walletClient,
+    watch: true,
+    onSuccess(data) {
+      setAllProjects(data);
+    },
+    onError(error) {
+      showAlert("Error", error);
+    },
+  });
+
+  const { config: configAdd, refetch: refetchAdd } = usePrepareContractWrite({
+    address: contractAddress,
+    abi: abi.abi,
+    functionName: "addProject",
+    from: walletClient?.account?.address,
+    args: [
+      iptImage.current?.value,
+      iptTitle.current?.value,
+      iptAbout.current?.value,
+      JSON.parse(iptKeywords.current?.value || JSON.stringify([])),
+      iptGitHub.current?.value,
+      iptWebsite.current?.value,
+    ],
+  });
+  const {
+    data: dataAdd,
+    write: writeAdd,
+    isLoading: loadingAdd,
+  } = useContractWrite(configAdd);
+
+  const { config: configRemove, refetch: refetchRemove } =
+    usePrepareContractWrite({
+      address: contractAddress,
+      abi: abi.abi,
+      functionName: "removeProject",
+      from: walletClient?.account?.address,
+      args: [removeId.current || 0],
+    });
+  const {
+    data: dataRemove,
+    write: writeRemove,
+    isLoading: loadingRemove,
+  } = useContractWrite(configRemove);
+
+  const { isLoading, isSuccess, isError, error } = useWaitForTransaction({
+    hash: dataAdd?.hash || dataRemove?.hash,
+  });
+
+  useEffect(() => {
+    if (removeId.current > -1) {
+      refetchRemove?.();
+      writeRemove?.();
+      removeId.current = undefined;
+    }
+  }, [removeId.current]);
+
+  useEffect(() => {
+    if (createNew.current) {
+      refetchAdd?.();
+      writeAdd?.();
+      createNew.current = false;
+    }
+  }, [createNew.current]);
+
+  useEffect(() => {
+    if (isSuccess || isError) {
+      showAlert(
+        isSuccess ? "Success" : "Error",
+        isSuccess
+          ? "Operation completed successfully"
+          : error?.message.substring(0, 40)
+      );
+    }
+  }, [isSuccess, isError]);
 
   function showAlert(title, text) {
     addAlert(title, text);
     addVariant(title);
   }
-
-  const addProject = async () => {
-    try {
-      if (iptTitle.current.value) {
-        setLoading(true);
-
-        const projectTxn = await contract.current.addProject(
-          iptImage.current.value,
-          iptTitle.current.value,
-          iptAbout.current.value,
-          JSON.parse(iptKeywords.current.value),
-          iptGitHub.current.value,
-          iptWebsite.current.value
-        );
-
-        await projectTxn.wait();
-
-        listAllProjects();
-        showAlert("Success", `Project has been added ${projectTxn.hash}`);
-      }
-    } catch (error) {
-      showAlert("Error", error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const removeProject = async (index) => {
-    try {
-      if (index) {
-        const removeProjectTxn = await contract.current.removeProject(index);
-
-        await removeProjectTxn.wait();
-
-        listAllProjects();
-        showAlert("Success", "The project has been removed");
-      }
-    } catch (error) {
-      showAlert("Error", error.message);
-    }
-  };
-
-  const listAllProjects = async () => {
-    try {
-      const listProjects = await contract.current.getAllProjects();
-
-      const projectsCleaned = listProjects.map((project) => {
-        return {
-          image: project.image,
-          title: project.title,
-          about: project.about,
-          keywords: project.keywords,
-          github: project.github,
-          website: project.website,
-        };
-      });
-
-      setAllProjects(projectsCleaned);
-    } catch (error) {
-      showAlert("Error", error.message);
-    }
-  };
-
-  async function getProvider() {
-    // @ts-ignore
-    alchemyProvider.current = new ethers.AlchemyProvider(
-      "goerli",
-      process.env.NEXT_PUBLIC_NETWORK_PROVIDER
-    );
-    contract.current = new ethers.Contract(
-      contractAddress,
-      contractABI,
-      await alchemyProvider.current
-    );
-
-    listAllProjects();
-  }
-
-  useEffect(() => {
-    getProvider();
-  }, []);
 
   return (
     <section className="min-h-[100vh] px-5 pt-[15vh] text-primaryText">
@@ -128,8 +133,14 @@ export function Admin() {
         />
         <Button
           title="Add Project"
-          onClick={() => addProject()}
-          disabled={loading}
+          onClick={async () => {
+            if (iptTitle.current?.value && iptKeywords.current?.value) {
+              createNew.current = true;
+            } else {
+              showAlert("Error", "Title and Keywords are required");
+            }
+          }}
+          disabled={isLoading || loadingAdd || loadingRemove}
         />
       </form>
 
@@ -139,11 +150,16 @@ export function Admin() {
           <ul className="mt-5">
             {allProjects.map((project, i) => {
               return (
-                <li key={project.title} className="mt-1">
-                  {i} - {project.title}
+                <li key={project.id} className="mt-1">
+                  {project.id} - {project.title}
                   <button
-                    className="ml-5 border-2 rounded-[10px] p-1 text-sm border-error text-error hover:border-errorHover hover:text-errorHover"
-                    onClick={() => removeProject(i)}
+                    className="ml-5 border-2 rounded-[10px] p-1 text-sm border-error 
+                    text-error hover:border-errorHover hover:text-errorHover
+                    disabled:border-cardHover disabled:text-cardHover"
+                    onClick={() => {
+                      removeId.current = i;
+                    }}
+                    disabled={isLoading || loadingAdd || loadingRemove}
                   >
                     Remove
                   </button>
